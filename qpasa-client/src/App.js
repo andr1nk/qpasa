729d92679a5aed79906a14f9114f33c310b7830d
Bianca <ing_biank@hotmail.com>
2019-05-08 16:28:34 +0200

    adds footer component

--- qpasa-client/src/App.js
+++ qpasa-client/src/App.js
@@ -2,12 +2,14 @@ import React from 'react'
 import './App.css'
 import Event from './components/Event'
 import Navbar from './components/Navbar'
+import Footer from './components/Footer'
 
 function App() {
     return (
         <div className="App">
             <Navbar />
             <Event />
+            <Footer />
         </div>
     )
 }
