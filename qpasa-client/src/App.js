e906ebee599e58cd81d5858a97df79d8b08e9fa9
Bianca <ing_biank@hotmail.com>
2019-05-08 16:18:54 +0200

    adds navbar component

--- qpasa-client/src/App.js
+++ qpasa-client/src/App.js
@@ -1,10 +1,12 @@
 import React from 'react'
 import './App.css'
 import Event from './components/Event'
+import Navbar from './components/Navbar'
 
 function App() {
     return (
         <div className="App">
+            <Navbar />
             <Event />
         </div>
     )
