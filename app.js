3a014609193786030a8d24fd28dc571e8133ae31
andr1nk <andre@avertu.ch>
2019-05-08 12:12:27 +0200

    changed to MONGODB_URI

--- app.js
+++ app.js
@@ -19,7 +19,7 @@ const session = require('express-session')
 const MongoStore = require('connect-mongo')(session)
 const flash = require('connect-flash')
 
-const mongoDBURL= process.env.NODE_ENV === "development" ?  process.env.MONGO_DEV_URL :  process.env.MONGO_PROD_URL
+const mongoDBURL= process.env.NODE_ENV === "development" ?  process.env.MONGO_DEV_URL :  process.env.MONGODB_URI
 
 mongoose
   .connect(mongoDBURL, {useNewUrlParser: true})
