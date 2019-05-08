4fc27743348065fd99d9c3df8455794e904cd424
Bianca <ing_biank@hotmail.com>
2019-05-08 15:23:56 +0200

    fetch data from Endpoint

--- qpasa-client/src/components/Event.js
+++ qpasa-client/src/components/Event.js
@@ -1,8 +1,46 @@
 import React from 'react'
+import axios from 'axios'
 
 class Event extends React.Component {
+    state = {
+        events: []
+    }
+
+    fetchData = () => {
+        axios.get('https://qpasa-api.herokuapp.com/api/events').then(response => {
+            this.setState({
+                events: response.data
+            })
+        })
+    }
+
+    componentDidMount() {
+        this.fetchData()
+    }
+
     render() {
-        return <h1>hola</h1>
+        return (
+            <div>
+                <div>
+                    {this.state.events.map(event => {
+                        return (
+                            <div key={event._id}>
+                                <h3>Event</h3>
+                                <h3>{event.name}</h3>
+                                <p>{event.description}</p>
+                                <p>{event.url}</p>
+                                <p>{event.date}</p>
+                                <h3>Location</h3>
+                                <p>{event.location.city}</p>
+                                <p>{event.location.name}</p>
+                                <p>{event.location.address}</p>
+                                <p>{event.location.GPS}</p>
+                            </div>
+                        )
+                    })}
+                </div>
+            </div>
+        )
     }
 }
 
