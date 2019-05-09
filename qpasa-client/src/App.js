3014b8f51589d5f1a056cc4fcc7111487edf45cb
Bianca <ing_biank@hotmail.com>
2019-05-09 12:21:24 +0200

    first mockup

--- qpasa-client/src/App.js
+++ qpasa-client/src/App.js
@@ -1,14 +1,12 @@
 import React from 'react'
 import './App.css'
-import Event from './components/Event'
-import Navbar from './components/Navbar'
 import Footer from './components/Footer'
+import Home from './components/Home'
 
 function App() {
     return (
-        <div className="App">
-            <Navbar />
-            <Event />
+        <div>
+            <Home />
             <Footer />
         </div>
     )
