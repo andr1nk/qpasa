e585ebe9b5679fc29be9d6e0e265efae428a3c74
Bianca <ing_biank@hotmail.com>
2019-05-14 18:26:10 +0200

    first proposal for days component

--- qpasa-client/src/components/Days.js
+++ qpasa-client/src/components/Days.js
@@ -2,11 +2,17 @@ import React from 'react'
 import EventList from './Events/EventList'
 
 class Days extends React.Component {
+    state = {
+        day: '14.05.2019'
+    }
+    onClickHandler = () => {}
     render() {
         console.log(this.props)
         return (
             <div>
-                <EventList pathname={this.props.location.pathname} />
+                <button value="17.05.2019"> day1</button>
+                <button> Day2 </button>
+                <EventList day={this.state.day} pathname={this.props.location.pathname} />
             </div>
         )
     }
