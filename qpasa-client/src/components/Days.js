8bfb05de31ea9a0b54098d92c283479d84caac1d
andr1nk <andre@avertu.ch>
2019-05-14 21:06:24 +0200

    show events per day finished

--- qpasa-client/src/components/Days.js
+++ qpasa-client/src/components/Days.js
@@ -1,17 +1,49 @@
 import React from 'react'
 import EventList from './Events/EventList'
+import moment from "moment";
+
+const day1 = moment(new Date()).format('DD.MM.YYYY')                    // Today
+const day1Str = moment(new Date()).format('dddd')                    // TodayString
+const day2 = moment(new Date()).add(1, 'day').format('DD.MM.YYYY')      // Tomorrow
+const day2Str = moment(new Date()).add(1, 'day').format('dddd')      // Tomorrow
+const day3 = moment(new Date()).add(2, 'day').format('DD.MM.YYYY')      // ...
+const day3Str = moment(new Date()).add(2, 'day').format('dddd')      // ...
+const day4 = moment(new Date()).add(3, 'day').format('DD.MM.YYYY')      // ...
+const day4Str = moment(new Date()).add(3, 'day').format('dddd')      // ...
+const day5 = moment(new Date()).add(4, 'day').format('DD.MM.YYYY')      // ...
+const day5Str = moment(new Date()).add(4, 'day').format('dddd')      // ...
+const day6 = moment(new Date()).add(5, 'day').format('DD.MM.YYYY')      // ...
+const day6Str = moment(new Date()).add(5, 'day').format('dddd')      // ...
+const day7 = moment(new Date()).add(6, 'day').format('DD.MM.YYYY')      // ...
+const day7Str = moment(new Date()).add(6, 'day').format('dddd')      // ...
 
 class Days extends React.Component {
     state = {
-        day: '14.05.2019'
+        day: day1
     }
-    onClickHandler = () => {}
+    
+    onClickHandler= (event)=> {
+        this.setState({
+            day:event.target.name
+        })
+        
+    }
+
     render() {
-        console.log(this.props)
         return (
             <div>
-                <button value="17.05.2019"> day1</button>
-                <button> Day2 </button>
+                <br/>
+                <br/>
+                <br/>
+                <br/>
+                <button type="button" onClick={(e)=>this.onClickHandler(e)} name={day1}> Today </button>
+                <button type="button" onClick={(e)=>this.onClickHandler(e)} name={day2}> Tomorrow </button>
+                <button type="button" onClick={(e)=>this.onClickHandler(e)} name={day3}> {day3Str}</button>
+                <button type="button" onClick={(e)=>this.onClickHandler(e)} name={day4}> {day4Str}</button>
+                <button type="button" onClick={(e)=>this.onClickHandler(e)} name={day5}> {day5Str}</button>
+                <button type="button" onClick={(e)=>this.onClickHandler(e)} name={day6}> {day6Str}</button>
+                <button type="button" onClick={(e)=>this.onClickHandler(e)} name={day7}> {day7Str}</button>
+
                 <EventList day={this.state.day} pathname={this.props.location.pathname} />
             </div>
         )
--- qpasa-client/src/components/Events/EventList.js
+++ qpasa-client/src/components/Events/EventList.js
@@ -3,19 +3,20 @@ import axios from 'axios'
 import './Event.css'
 import Event from './Event'
 
+
 class EventList extends React.Component {
     state = {
         events: []
     }
 
     fetchData = () => {
-        axios.get(`${process.env.REACT_APP_SERVER_URL}/api/events`).then(response => {
-            this.setState({
-                events: response.data
+        axios.get(`${process.env.REACT_APP_SERVER_URL}/api/events`)
+            .then(response => {
+                this.setState({
+                    events: response.data
             })
         })
     }
-    //nada
 
     componentDidMount() {
         this.fetchData()
@@ -27,27 +28,14 @@ class EventList extends React.Component {
                 <div className="container event-container">
                     {this.props.pathname === '/events-zurich'
                         ? this.state.events
-                              .filter(event => {
-                                  return event.location.city === 'Zürich'
-                              })
-                              .map(event => {
-                                  return (
-                                      <div>
-                                          <Event event={event} key={event._id} />
-                                      </div>
-                                  )
-                              })
+                            .sort((a, b) => a.location.name.localeCompare(b.location.name))
+                            .filter(event => event.location.city === 'Zürich' && event.date === this.props.day)
+                            .map(event => <div> <Event event={event} key={event._id} /> </div>)
                         : this.state.events
-                              .filter(event => {
-                                  return event.location.city === 'Berlin'
-                              })
-                              .map(event => {
-                                  return (
-                                      <div>
-                                          <Event event={event} key={event._id} />
-                                      </div>
-                                  )
-                              })}
+                            .sort((a, b) => a.location.name.localeCompare(b.location.name))
+                            .filter(event => event.location.city === 'Berlin' && event.date === this.props.day)
+                            .map(event => <div><Event event={event} key={event._id} /></div>)
+                    }
                 </div>
             </div>
         )
