c06a66ed3a70f870188820d9bbaf63c7b3e72abf
Bianca <ing_biank@hotmail.com>
2019-05-14 12:20:42 +0200

    adds css

--- qpasa-client/src/App.js
+++ qpasa-client/src/App.js
@@ -1,5 +1,6 @@
 import React from 'react'
 import './App.css'
+import 'mapbox-gl/dist/mapbox-gl.css'
 import Navbar from './components/Navbar'
 import Footer from './components/Footer'
 import Home from './components/Home'
@@ -10,32 +11,31 @@ import AddLocation from './components/Location/Add'
 import Signup from './components/Auth/Signup'
 import Login from './components/Auth/Login'
 import { loggedin } from './services/auth'
-import ProtectedRoute from "./components/ProtectedRoute";
+import ProtectedRoute from './components/ProtectedRoute'
 
 import { Switch, Route } from 'react-router-dom'
 
 class App extends React.Component {
-
     state = {
         loggedIn: null
-    };
+    }
 
     setUser = user => {
         this.setState({
             loggedIn: user
-        });
-    };
+        })
+    }
 
     getUser = () => {
         loggedin().then(user => {
             this.setState({
                 loggedIn: user
-            });
-        });
-    };
+            })
+        })
+    }
 
     componentDidMount() {
-        this.getUser();
+        this.getUser()
     }
 
     render() {
@@ -50,29 +50,25 @@ class App extends React.Component {
                         path="/signup"
                         //  component={Signup}
                         //  render allows us to pass props to our component inside Routes
-                        render={(props) => <Signup {...props} setUser={this.setUser} />}
+                        render={props => <Signup {...props} setUser={this.setUser} />}
                     />
                     <Route
                         exact
                         path="/login"
                         //  component={login}
                         //  render allows us to pass props to our component inside Routes
-                        render={(props) => <Login {...props} setUser={this.setUser} />}
+                        render={props => <Login {...props} setUser={this.setUser} />}
                     />
                     <Route
                         exact
                         path="/locations/:id"
-                        render={props => (
-                            <LocationDetails {...props} user={this.state.loggedIn} />
-                        )}
+                        render={props => <LocationDetails {...props} user={this.state.loggedIn} />}
                     />
                     <Route
                         exact={true}
                         path="/locations"
                         user={this.state.loggedIn}
-                        render={props => (
-                            <LocationList {...props} user={this.state.loggedIn}/>
-                        )}
+                        render={props => <LocationList {...props} user={this.state.loggedIn} />}
                     />
                 </Switch>
                 <Footer />
--- qpasa-client/src/components/Map.js
+++ qpasa-client/src/components/Map.js
@@ -8,18 +8,19 @@ class Location extends React.Component {
             height: 300,
             latitude: parseFloat(this.props.latitude),
             longitude: parseFloat(this.props.longitude),
-            zoom: 14
+            zoom: 15
         }
     }
 
     render() {
         return (
             <ReactMapGL
+                mapStyle="mapbox://styles/mapbox/bright-v8"
                 mapboxApiAccessToken={process.env.MAPBOX_ACCESS_TOKE}
                 {...this.state.viewport}
                 onViewportChange={viewport => this.setState({ viewport })}
             >
-                <Marker latitude={this.state.viewport.latitude} longitude={this.state.viewport.longitude}>
+                <Marker latitude={this.state.viewport.latitude} longitude={this.state.viewport.longitude} draggable>
                     <i className="fas fa-map-marker-alt" />
                 </Marker>
             </ReactMapGL>
