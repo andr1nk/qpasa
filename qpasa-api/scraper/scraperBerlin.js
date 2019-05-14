710446aff4838211d7a3452a4e705facaba65006
andr1nk <andre@avertu.ch>
2019-05-14 12:40:28 +0200

    scraper with waitUntil:load

--- qpasa-api/scraper/scraperBerlin.js
+++ qpasa-api/scraper/scraperBerlin.js
@@ -10,7 +10,7 @@ const scrapeResidentAdvisorBerlin = async (day_residentAdvisorFormat, day_qpasaF
 
     await page.goto(`https://www.residentadvisor.net/events/de/berlin/day/${day_residentAdvisorFormat}`,
         {
-            timeout: 3000000
+            waitUntil: "load"
         });
     const scrapedData = await page.evaluate(() =>
         Array.from(document.querySelectorAll("div.bbox")).map( link =>  ({
