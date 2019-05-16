195c687269cd0d2796f3565a7b34255a7281a88f
andr1nk <andre@avertu.ch>
2019-05-16 19:58:52 +0200

    minor changes

--- qpasa-client/src/components/Home.js
+++ qpasa-client/src/components/Home.js
@@ -10,18 +10,13 @@ class Home extends React.Component {
         return (
             <div>
                 <header className="teaser">
-                    <p className="teaser-heading">Q'pasa</p>
+                    <p className="teaser-heading">¿qpasa?</p>
                 </header>
                 <div className="container">
                     <div className="col-md-10 offset-md-1">
                         <p className="description">
-                            Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed diam nonumy eirmod tempor
-                            invidunt ut labore et dolore magna aliquyam erat, sed diam voluptua. At vero eos et accusam
-                            et justo duo dolores et ea rebum. Stet clita kasd gubergren, no sea takimata sanctus est
-                            Lorem ipsum dolor sit amet. Lorem ipsum dolor sit amet, consetetur sadipscing elitr, sed
-                            diam nonumy eirmod tempor invidunt ut labore et dolore magna aliquyam erat, sed diam
-                            voluptua. At vero eos et accusam et justo duo dolores et ea rebum. Stet clita kasd
-                            gubergren, no sea takimata sanctus est Lorem ipsum dolor sit amet.
+                            <h2 style={{textAlign: "center"}} >All your favourite parties with one click.</h2>
+                            <br/>
                         </p>
                         <div className="buttons">
                             <button className="btn btn-lg btn-dark button">
